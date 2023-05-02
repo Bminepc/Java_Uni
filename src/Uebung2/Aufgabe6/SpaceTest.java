9e910963a11ce69bf8946ae0e5042ab4597b2889
Benjamin Wendt <95280764+Bminepc@users.noreply.github.com>
2023-05-02 11:21:23 +0200

    Initializing of testobjects

--- src/Uebung2/Aufgabe6/SpaceTest.java
+++ src/Uebung2/Aufgabe6/SpaceTest.java
@@ -5,10 +5,16 @@ public class SpaceTest {
 
     public static void main(String[] args) {
 
+        //Initialisieren
         Stern sonne = new Stern("Sonne", 1200000000, 120000000000.0, Stern.Spektralklasse.sk_G, Stern.Leuchtkraftklasse.lk_I );
-        Planet[] planetes = new Planet[8];
-        planetes[0] = new Gesteinsplanet("Merkur", 4879.0, 120000.0, sonne, 0, null, 0);
-        Gesteinsplanet erdmond = new Gesteinsplanet("Erdmond", 3.0, 23.9, )
-        planetes[1] = new
+        Planet[] planetes = new Planet[2];
+        planetes[0] = new Gesteinsplanet("Merkur", 4879.0, 120000.0, sonne, 0, 0);
+        planetes[1] = new Gesteinsplanet("Erde", 12756.0, 12000.0, sonne, 1, 8000000);
+        Gesteinsplanet erdmond = new Gesteinsplanet("Erdmond", 3.0, 23.9, planetes[1], 0, 0);
+        planetes[1].addMond(erdmond, 0);
+        Kleinkörper[] kleinkörper = new Kleinkörper[3];
+        kleinkörper[0] = new Komet("Seltsamer1", 1, 1.0, sonne, 34.0);
+        kleinkörper[1] = new Komet("Seltsamer2", 2, 2.0, sonne, 34.0);
+        kleinkörper[2] = new Asteroid("Asto", 30, 120000.0, sonne, true);
     }
 }
