456558a86f855ca8c09495802f461a9136f693c3
Benjamin Wendt <95280764+Bminepc@users.noreply.github.com>
2023-05-02 11:00:09 +0200

    Started adding required tests

--- src/Uebung2/Aufgabe6/SpaceTest.java
+++ src/Uebung2/Aufgabe6/SpaceTest.java
@@ -2,9 +2,13 @@ package Uebung2.Aufgabe6;
 
 
 public class SpaceTest {
-    public static final int MONDE_MAX = 180;
 
     public static void main(String[] args) {
 
+        Stern sonne = new Stern("Sonne", 1200000000, 120000000000.0, Stern.Spektralklasse.sk_G, Stern.Leuchtkraftklasse.lk_I );
+        Planet[] planetes = new Planet[8];
+        planetes[0] = new Gesteinsplanet("Merkur", 4879.0, 120000.0, sonne, 0, null, 0);
+        Gesteinsplanet erdmond = new Gesteinsplanet("Erdmond", 3.0, 23.9, )
+        planetes[1] = new
     }
 }
