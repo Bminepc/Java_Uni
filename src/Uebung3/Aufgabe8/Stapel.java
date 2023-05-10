dde12074655091d6672826dba369ebc681b322b6
Mathis Steinhage <2156931@uni-wuppertal.de>
2023-05-10 11:09:29 +0200

    Ich bin wütend

--- src/Uebung3/Aufgabe8/Stapel.java
+++ src/Uebung3/Aufgabe8/Stapel.java
@@ -3,27 +3,29 @@ package Uebung3.Aufgabe8;
 public class Stapel<T> {
     private Element<T> erstesElement;
     public Stapel(){
-        erstesElement= null;
+        erstesElement = new Element<T>(null, null);
+
     }
     public void ablegen(T e){
-        Element<T> temp = erstesElement;
-        erstesElement.setDaten(e);
-        erstesElement.setNachfolger(temp);
+        if(erstesElement != null) {
+            Element<T> nachfolger = new Element<T>(erstesElement.getDaten(), erstesElement.getNachfolger());
+            erstesElement.setDaten(e);
+            erstesElement.setNachfolger(nachfolger);
+        }else{
+            erstesElement = new Element<T>(e, null);
+        }
     }
 
-    public T entnehmen() {
+    public T entnehmen() throws Exception{
         T ausgabeDaten = erstesElement.getDaten();
         if (!this.leer()) {
             erstesElement.setDaten(erstesElement.getNachfolger().getDaten());
             erstesElement.setNachfolger(erstesElement.getNachfolger().getNachfolger());
-            return ausgabeDaten;
         }
         else{
-            System.out.println("Der Stack ist leer");
-            return null;
+            throw new Exception("Der Stack ist leer");
         }
+        return ausgabeDaten;
     }
-    public boolean leer(){
-        return erstesElement == null;
-    }
+    public boolean leer(){return erstesElement.getDaten() == null;}
 }
--- src/Uebung3/Aufgabe8/TestStapel.java
+++ src/Uebung3/Aufgabe8/TestStapel.java
@@ -1,4 +1,56 @@
 package Uebung3.Aufgabe8;
 
 public class TestStapel {
+    public static void main(String[] args){
+        System.out.println("Test für Datentyp int:");
+        Stapel<Integer> IntStack = new Stapel<>();
+        //Testen von Methode leer()
+        System.out.println(IntStack.leer());
+        //Testen von Methode ablegen()
+        IntStack.ablegen(5);
+        IntStack.ablegen(19);
+        //Testen von Methode entnehmen()
+        try{
+            IntStack.entnehmen();
+        }catch (Exception a) {
+            System.out.println(a.getMessage());
+        }
+        try{
+            IntStack.entnehmen();
+        }catch (Exception a){
+            System.out.println(a.getMessage());
+        }
+        //Testen der Exception bei Aufruf von entnehmen() bei leerem Stack
+        try{
+            IntStack.entnehmen();
+        }catch (Exception a){
+            System.out.println(a.getMessage());
+        }
+
+        System.out.println("-----------------------");
+        System.out.println("Test für Datentyp char:");
+        Stapel<Character> CharStack = new Stapel<>();
+        //Testen von Methode leer()
+        System.out.println(CharStack.leer());
+        //Testen von Methode ablegen()
+        CharStack.ablegen('x');
+        CharStack.ablegen('?');
+        //Testen von Methode entnehmen()
+        try{
+            CharStack.entnehmen();
+        }catch (Exception a) {
+            System.out.println(a.getMessage());
+        }
+        try{
+            CharStack.entnehmen();
+        }catch (Exception a){
+            System.out.println(a.getMessage());
+        }
+        //Testen der Exception bei Aufruf von Methode entnehmen() bei leeren Stack
+        try{
+            CharStack.entnehmen();
+        }catch (Exception a){
+            System.out.println(a.getMessage());
+        }
+    }
 }
