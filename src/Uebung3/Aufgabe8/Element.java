168970f34aa7ede3abac782bcf68dceb43430b92
Mathis Steinhage <2156931@uni-wuppertal.de>
2023-05-11 12:16:09 +0200

    Kommentare...

--- src/Uebung3/Aufgabe8/Element.java
+++ src/Uebung3/Aufgabe8/Element.java
@@ -3,24 +3,25 @@ package Uebung3.Aufgabe8;
 public class Element<T> {
     private T daten;
     private Element<T> nachfolger;
-
+    //Konstruktor für Klasse Element
     public Element(T d, Element<T> n){
         this.daten = d;
         this.nachfolger = n;
     }
+    //Standardkonstruktor für Klasse Element
     public Element(){
         this.daten = null;
         this.nachfolger = null;
     }
-    public T getDaten(){
-        return this.daten;
-    }
-    public void setDaten(T d){
-        this.daten = d;
-    }
+    //Methode zur Rückgabe von Attribut daten
+    public T getDaten(){return this.daten;}
+    //Methode zum Ändern des Attributs daten
+    public void setDaten(T d){this.daten = d;}
+    //Methode zur Rückgabe von Attribut nachfolger
     public Element<T> getNachfolger(){
         return this.nachfolger;
     }
+    //Methode zum Ändern des Attributs nachfolger
     public void setNachfolger(Element<T> n){
         this.nachfolger = n;
     }
--- src/Uebung3/Aufgabe8/Stapel.java
+++ src/Uebung3/Aufgabe8/Stapel.java
@@ -2,23 +2,27 @@ package Uebung3.Aufgabe8;
 
 public class Stapel<T> {
     private Element<T> erstesElement;
-    //Konstruktor für Klasse Stapel
+    //Standardkonstruktor für Klasse Stapel
     public Stapel(){ erstesElement = new Element<T>(null, null);}
     public void ablegen(T e){
         //Falls Stack bereits gefüllt
         if(erstesElement != null) {
+            //erstelle Nachfolgeelement, aktualisiere Daten von erstesElement und Nachfolgeelement
             Element<T> nachfolger = new Element<T>(erstesElement.getDaten(), erstesElement.getNachfolger());
             erstesElement.setDaten(e);
             erstesElement.setNachfolger(nachfolger);
         //Falls Stack leer
         }else{
+            //aktualisiere erstesElement mit neuen Daten
             erstesElement = new Element<T>(e, null);
         }
     }
 
     public T entnehmen() throws Exception{
+        //speichere Daten von erstesElement
         T ausgabeDaten = erstesElement.getDaten();
         if (!this.leer()) {
+            //aktualisiere Daten von erstesElement mit Nachfolgeelement
             erstesElement.setDaten(erstesElement.getNachfolger().getDaten());
             erstesElement.setNachfolger(erstesElement.getNachfolger().getNachfolger());
         }
@@ -26,7 +30,9 @@ public class Stapel<T> {
         else{
             throw new Exception("Der Stack ist leer");
         }
+        //gebe gespeicherte Daten aus
         return ausgabeDaten;
     }
+    //Methode gibt zurück, ob der Stack leer ist
     public boolean leer(){return erstesElement.getDaten() == null;}
 }
