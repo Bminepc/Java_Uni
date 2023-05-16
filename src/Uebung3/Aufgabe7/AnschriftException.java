d761ca348d3deb06a43c7ffef1c6a58c8dedfa5a
Benjamin Wendt <95280764+Bminepc@users.noreply.github.com>
2023-05-16 10:03:18 +0200

    Added custom exception

--- src/Uebung3/Aufgabe7/AnschriftException.java
+++ src/Uebung3/Aufgabe7/AnschriftException.java
@@ -1,4 +1,45 @@
 package Uebung3.Aufgabe7;
 
 public class AnschriftException extends Exception{
+    enum FehlerTyp{
+        ORTFEHLER,
+        PLZFEHLER,
+        ZUSATZFEHLER,
+        UNDEFINED;
+    }
+
+    private FehlerTyp error;
+
+    public AnschriftException(){
+        error = FehlerTyp.UNDEFINED;
+    }
+
+    public AnschriftException(FehlerTyp Error) {
+        error = Error;
+    }
+
+    public FehlerTyp getExceptioinCode(){
+        return error;
+    }
+
+    @Override
+    public String toString(){
+        switch (error){
+            case ORTFEHLER : {
+                return "Beim einlesen den Ortes wurde ein Fehler gefunden.";
+            }
+            case PLZFEHLER : {
+                return "Bei einlesen der Postleitzahl wurde ein Fehler gefunden.";
+            }
+            case UNDEFINED :{
+                return "Es ist ein nicht definierter Fehler aufgetreten.";
+            }
+            case ZUSATZFEHLER : {
+                return "Bei einlesen des Adresszusatzes ist ein Fehler aufgetreten.";
+            }
+            default: {
+                return "Unexpected Error. Please contact your admin.";
+            }
+        }
+    }
 }
