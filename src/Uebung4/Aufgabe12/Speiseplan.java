8c6e685341af3afab2e4e850369c8e82cf51a1c2
Benjamin Wendt <95280764+Bminepc@users.noreply.github.com>
2023-06-06 11:35:59 +0200

    More tests

--- src/Uebung4/Aufgabe12/Speiseplan.java
+++ src/Uebung4/Aufgabe12/Speiseplan.java
@@ -3,6 +3,7 @@ package Uebung4.Aufgabe12;
 import Uebung4.Aufgabe11.FarbigePixel;
 
 import javax.swing.*;
+import java.io.*;
 
 public class Speiseplan extends JFrame{
 
@@ -10,10 +11,24 @@ public class Speiseplan extends JFrame{
     public Speiseplan(){
         setResizable(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        args[0];
     }
 
     public static void main(String[] args){
         SwingUtilities.invokeLater(Speiseplan::new);
+        try (FileReader fr = new FileReader(new File(args[0]));
+             BufferedReader br = new BufferedReader(fr))
+        {
+            System.out.println("Inhalt␣von␣" + args[0]);
+            String zeile = br.readLine();
+            while (zeile != null)
+            {
+                System.out.println(zeile);
+                zeile = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
