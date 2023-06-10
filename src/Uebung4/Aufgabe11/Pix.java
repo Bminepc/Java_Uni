9139b44d15155eaff8ddddf6d17824ea54ce10d9
Mathis Steinhage <mathissteinhage@gmx.de>
2023-06-10 13:42:21 +0200

    Kommentare für Aufgabe 11

--- src/Uebung4/Aufgabe11/Pix.java
+++ src/Uebung4/Aufgabe11/Pix.java
@@ -2,14 +2,17 @@ package Uebung4.Aufgabe11;
 
 public class Pix {
     private java.awt.Color Farbe;
+    //zwei Attribute für Koordinaten des Pixels
     private int x;
     private int y;
 
+    //Konstruktor
     public Pix(java.awt.Color f, int X, int Y){
         this.Farbe = f;
         this.x = X;
         this.y = Y;
     }
+    //getter für alle Attribute der Klasse
 
     public java.awt.Color getFarbe(){return this.Farbe;}
     public int getX(){return this.x;}
