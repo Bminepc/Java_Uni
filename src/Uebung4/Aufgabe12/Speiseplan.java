275d0785416e1890ecd051819faa094caa743312
Benjamin Wendt <95280764+Bminepc@users.noreply.github.com>
2023-06-12 01:11:11 +0200

    To Mathis: Read Comments

--- src/Uebung4/Aufgabe12/Speiseplan.java
+++ src/Uebung4/Aufgabe12/Speiseplan.java
@@ -8,11 +8,13 @@ public class Speiseplan extends JFrame{
 
 
     public static void main(String[] args){
+        // Erstellen einer Arraylist um für eine variable Größe der Eingangsdaten vorbereitet zu sein
         ArrayList<String> rawData = new ArrayList<String>();
+        // Filereader und Bufferedreader als automatisch gemanaged öffnen und ein Try-Catch für deren Fehler erstellen.
         try (FileReader fr = new FileReader(new File(args[0]));
              BufferedReader br = new BufferedReader(fr))
         {
-            System.out.println("Inhalt␣von␣" + args[0]);
+            // Einlesen aller Zeilen und hinzufügen zu der oben erstellenen Arraylist.
             String zeile = br.readLine();
             while (zeile != null)
             {
@@ -24,22 +26,37 @@ public class Speiseplan extends JFrame{
         } catch (IOException e) {
             e.printStackTrace();
         }
+        // Erstellen eines Speiseplans (und selbst) als JFrame der Software
         Speiseplan f = new Speiseplan();
 
+        // Umwandeln der Daten in Stringarrays zur einfachen Nutzung
+
+        // Umwandlung der theoretisch unbekannt langen Arraylist in ein Stringarray mit fester Größe
         String[] datasets = new String[rawData.size()];
         for( int i = 0; i<rawData.size();i++){
             datasets[i] = rawData.get(i);
         }
+        // Aufteilen der als CSV formatierten Daten in einzelne Datensätze dank der String.split Methode.
+
+        // Ein Stringarray als Refferenz erstellen
         String[] c = datasets[0].split(";");
+        // Das zweidimensionale Array für die Tabelle instanziieren
         String[][] dataset = new String[datasets.length][c.length];
+        // Das Array mit den entsprechenden Daten füllen
         for (int i = 0; i<dataset.length;i++){
             dataset[i] = datasets[i].split(";");
         }
 
+        // Einen JTable erstellen mit dem Konstruktor für zweidimensionale Arrays als Daten
         JTable table = new JTable(dataset,dataset[0]);
+        // Hinzufügen der Tables zu unserem Fenster
         f.add(table);
+        // Aufräumen
         f.pack();
+        // Quick and Dirty but works
+        // Hier wird die Größe so angepasst, dass alles zu lesen ist. Es gibt mit Sicherheit eine schönere Möglichkeit, aber diese sollte in jedem Fall zum gewünschten Ergebniss führen. (Wie es richtig geht wird bald per Lösung aus der Übung ergänzt)
         f.setSize(800,200);
+        // Alles sichtbar machen und zufrieden sein
         f.setVisible(true);
     }
 }
