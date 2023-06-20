054addff7881106312f967ef0bdb0e9a2aaddd59
Benjamin Wendt <95280764+Bminepc@users.noreply.github.com>
2023-06-20 10:09:02 +0200

    Bon Bon le Bon Bon

--- src/Uebung5/Aufgabe14/BonbonGlas.java
+++ src/Uebung5/Aufgabe14/BonbonGlas.java
@@ -2,22 +2,24 @@ package Uebung5.Aufgabe14;
 
 public class BonbonGlas {
     private int anzahl_bonbons;
+    boolean verfuegbar = true;
 
 
-    public BonbonGlas(int a){
-        this.anzahl_bonbons = a;
+    public BonbonGlas(int anzahl){
+        this.anzahl_bonbons = anzahl;
     }
     public synchronized void bonbonNehmen(){
-        boolean verfuegbar = true;
         while(!verfuegbar){
             try{wait();}
             catch (InterruptedException e) {}
         }
         verfuegbar = false;
+        anzahl_bonbons--;
+        verfuegbar = true;
         notify();
 
     }
     public String toString(){
-        return null;
+        return " " + anzahl_bonbons;
     }
 }
