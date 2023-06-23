b86516e673fb1344a8335657f8e5199cca66712f
Bminepc <benjamin.wen@outlook.com>
2023-06-23 14:40:37 +0200

    Comments

--- src/Uebung5/Aufgabe15/Balken.java
+++ src/Uebung5/Aufgabe15/Balken.java
@@ -2,13 +2,14 @@ package Uebung5.Aufgabe15;
 
 import javax.swing.*;
 
-public class Balken extends JProgressBar { // TODO Warum haben wir hierfür eine Extraklasse?
+public class Balken extends JProgressBar {
+    // Aufruf des Konstruktors der Elternklasse
     public Balken(int Orientation, int min, int max) {
         super(Orientation, min, max);
     }
 
     public synchronized void proceed() {
-        this.setValue(this.getValue() + 1);
+        this.setValue(this.getValue() + 1); // Fortschreiten des Balkens um genau eine Einheit
     }
 
 }
