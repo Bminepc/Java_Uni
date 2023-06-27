92c19993a581c02b2b1b050c5bb078b16e0e50e2
Benjamin Wendt <95280764+Bminepc@users.noreply.github.com>
2023-06-27 12:00:06 +0200

    Funktionierende einseitige Kommunikation

--- src/Uebung6/Aufgabe16/KugelClient.java
+++ src/Uebung6/Aufgabe16/KugelClient.java
@@ -1,16 +1,23 @@
 package Uebung6.Aufgabe16;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class KugelClient {
     public static void main(String[] args) throws IOException {
         try {
-            Socket kugelSocket = new Socket("132.195.45.233", 52390);
+            Socket kugelSocket = new Socket("localhost", 52390);
+            InputStream clientIn = kugelSocket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(clientIn);
+            BufferedReader reader = new BufferedReader(isr);
+            System.out.println(reader.readLine());
         }catch (IOException ioe) {
             System.out.println(ioe.getMessage());
         }
-        System.out.println("B");
+
 
     }
 }
--- src/Uebung6/Aufgabe16/KugelServer.java
+++ src/Uebung6/Aufgabe16/KugelServer.java
@@ -1,6 +1,9 @@
 package Uebung6.Aufgabe16;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,9 +13,21 @@ public class KugelServer {
 
     public static void main(String[] args) throws IOException {
         while (true) {
-            ServerSocket p = new ServerSocket(port);
-            Socket c = p.accept();
-            System.out.println(c.getRemoteSocketAddress());
+            try {
+                ServerSocket p = new ServerSocket(port);
+                Socket c = p.accept();
+                OutputStream serverOut = c.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(serverOut);
+                BufferedWriter writer = new BufferedWriter(osw);
+                writer.write("Bananas");
+                writer.newLine();
+                writer.flush();
+                c.close();
+                // Bad practice!
+                p.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
