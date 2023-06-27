256b83b94f634c5f2e6cef96afd99f47f05a3fc1
Benjamin Wendt <95280764+Bminepc@users.noreply.github.com>
2023-06-27 14:17:51 +0200

    Ohne Packages

---
+++ Abgaben ohne Package/Aufgabe16/Kreissaver.java
@@ -0,0 +1,25 @@
+
+import java.awt.*;
+
+public class Kreissaver {
+    private double x,y;
+    private Color c;
+
+    public Kreissaver(double x, double y, Color c){
+        this.x = x;
+        this.y = y;
+        this.c = c;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public Color getColor(){
+        return c;
+    }
+}
---
+++ Abgaben ohne Package/Aufgabe16/KugelClient.java
@@ -0,0 +1,42 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class KugelClient {
+    public static void main(String[] args) throws IOException {
+        int x = 0, y = 0;
+        double kx = 0.0, ky = 0.0;
+        Color c = new Color(0,0,0);
+        JFrame frame = new JFrame("Kreis");
+        ClientPanel panel = new ClientPanel();
+        try {
+            Socket kugelSocket = new Socket("localhost", 52390);
+            InputStream clientIn = kugelSocket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(clientIn);
+            BufferedReader reader = new BufferedReader(isr);
+            x = Integer.parseInt(reader.readLine());
+            y = Integer.parseInt(reader.readLine());
+            while (reader.readLine().equals("Has Next")) {
+                kx = Double.parseDouble(reader.readLine());
+                ky = Double.parseDouble(reader.readLine());
+                c = new Color(Integer.parseInt(reader.readLine()), Integer.parseInt(reader.readLine()), Integer.parseInt(reader.readLine()));
+                panel.addKreis(kx,ky,c);
+            }
+
+        }catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+        panel.setSize(x,y);
+        panel.repaint();
+        frame.add(panel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(x,y);
+        frame.setVisible(true);
+    }
+}
