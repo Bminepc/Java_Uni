5bedb47d7751b16dd644eb525cf569d1189de90f
Benjamin Wendt <95280764+Bminepc@users.noreply.github.com>
2023-06-27 11:39:32 +0200

    Server v1

--- src/Uebung6/Aufgabe16/KugelServer.java
+++ src/Uebung6/Aufgabe16/KugelServer.java
@@ -1,4 +1,18 @@
 package Uebung6.Aufgabe16;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class KugelServer {
+
+    private static int port = 52390;
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            ServerSocket p = new ServerSocket(port);
+            Socket c = p.accept();
+            System.out.println(c.getRemoteSocketAddress());
+        }
+    }
 }
