c9f2fe8ada8a9930c093f96fd994d53f5e52f4e8
Mathi <mathissteinhage@gmx.de>
2023-06-27 11:39:57 +0200

    Uebung6: Ordnerstruktur angelegt

--- src/Uebung6/Aufgabe16/KugelClient.java
+++ src/Uebung6/Aufgabe16/KugelClient.java
@@ -1,4 +1,16 @@
 package Uebung6.Aufgabe16;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class KugelClient {
+    public static void main(String[] args) throws IOException {
+        try {
+            Socket kugelSocket = new Socket("132.195.45.233", 52390);
+        }catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+        System.out.println("B");
+
+    }
 }
