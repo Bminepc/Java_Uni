478b45f360cd1a2fe5ee7318a1885b89f3570f60
Benjamin Wendt <95280764+Bminepc@users.noreply.github.com>
2023-06-27 12:54:12 +0200

    Server speichert Punkte und sendet alle raus

--- src/Uebung6/Aufgabe16/KugelServer.java
+++ src/Uebung6/Aufgabe16/KugelServer.java
@@ -7,6 +7,7 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class KugelServer {
@@ -14,6 +15,7 @@ public class KugelServer {
     private static int port = 52390;
 
     public static void main(String[] args) throws IOException {
+        ArrayList<Kreissaver> kreise = new ArrayList<>();
         Random r = new Random();
         while (true) {
             try {
@@ -23,18 +25,21 @@ public class KugelServer {
                 OutputStreamWriter osw = new OutputStreamWriter(serverOut);
                 BufferedWriter writer = new BufferedWriter(osw);
                 // Datenerzeugung
-                int x = r.nextInt(100,1000);
-                int y = r.nextInt(100,1000);
-                double kx = r.nextDouble(x-10);
-                double ky = r.nextDouble(y-10);
+                int x = 600;
+                int y = 600;
+                double kx = r.nextDouble(50,550);
+                double ky = r.nextDouble(50,550);
                 Color color = Color.getHSBColor(r.nextFloat(100), 0.9F, 0.8F);
+                kreise.add(new Kreissaver(kx,ky,color));
                 writer.write(x + "\n");
                 writer.write(y + "\n");
-                writer.write("" + kx + "\n");
-                writer.write("" + ky + "\n");
-                writer.write(color.getRed() + "\n");
-                writer.write(color.getGreen() + "\n");
-                writer.write(color.getBlue() + "\n");
+                for(int i = 0; i < kreise.size();i++){
+                    writer.write(kreise.get(i).getX() + "\n");
+                    writer.write(kreise.get(i).getY() + "\n");
+                    writer.write(kreise.get(i).getColor().getRed() + "\n");
+                    writer.write(kreise.get(i).getColor().getGreen() + "\n");
+                    writer.write(kreise.get(i).getColor().getBlue() + "\n");
+                }
                 System.out.println("Have written to Client");
                 writer.newLine();
                 writer.flush();
