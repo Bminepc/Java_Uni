6bd3d28f4478deb2d4fc582364bfa9ccdcbb1b0b
Mathi <mathissteinhage@gmx.de>
2023-06-28 10:42:53 +0200

    Aufgabe 18: Grundstruktur angelegt, bisher nicht funktional

--- src/Uebung6/Aufgabe18/UDPReceiver.java
+++ src/Uebung6/Aufgabe18/UDPReceiver.java
@@ -1,4 +1,32 @@
 package Uebung6.Aufgabe18;
 
+import java.io.IOException;
+import java.net.*;
+
+import static Uebung6.Aufgabe18.UDPReceiver.createMessage;
+
 public class UDPReceiver {
+    public static void createMessage(byte[] list, String i){
+        list[0] = Byte.parseByte(i);
+        for (int j = 1; j < 65001; j++) {
+            list[j] = Byte.parseByte(" ");
+        }
+    }
+    public static void sendPackage(String host, int port, int i) throws SocketException {
+        byte[] message = new byte[65001];
+        createMessage(message, Integer.toString(i));
+        try (DatagramSocket ds = new DatagramSocket())
+            {
+                InetAddress address = InetAddress.getByName(host);
+                DatagramPacket pack = new DatagramPacket(message, message.length, address, port);
+                ds.send(pack);
+            } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void main(String[] args) throws SocketException {
+        for (int i = 0; i < 100; i++) {
+            sendPackage(args[0], Integer.parseInt(args[1]), i);
+        }
+    }
 }
--- src/Uebung6/Aufgabe18/UDPSender.java
+++ src/Uebung6/Aufgabe18/UDPSender.java
@@ -1,4 +1,22 @@
 package Uebung6.Aufgabe18;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 public class UDPSender {
+    public static void main(String[] args){
+        int receivedPacks = 0;
+        try (DatagramSocket ds = new DatagramSocket(Integer.parseInt(args[0])))
+        {
+            while (true) {
+                DatagramPacket pack = new DatagramPacket(new byte[65001], 65001);
+                ds.receive(pack);
+                String message = String.valueOf(pack.getData()[0]) + String.valueOf(pack.getData()[1]) + String.valueOf(pack.getData()[2]);
+                System.out.println("Pack " + receivedPacks + ": " + message);
+                receivedPacks++;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
