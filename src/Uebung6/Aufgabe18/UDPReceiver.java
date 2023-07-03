8fa8cee78292cb0c5583102398e3a8a3c7f92145
Bminepc <benjamin.wen@outlook.com>
2023-07-03 14:58:53 +0200

    Made working Timeout after 10 Seconds

--- src/Uebung6/Aufgabe18/UDPReceiver.java
+++ src/Uebung6/Aufgabe18/UDPReceiver.java
@@ -3,11 +3,14 @@ package Uebung6.Aufgabe18;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketTimeoutException;
+
 public class UDPReceiver {
     public static void main(String[] args){
         int receivedPacks = 0;
         try (DatagramSocket ds = new DatagramSocket(Integer.parseInt(args[0])))
         {
+            ds.setSoTimeout(10000);
             while (true) {
                 DatagramPacket pack = new DatagramPacket(new byte[65001], 65001);
                 ds.receive(pack);
@@ -24,6 +27,8 @@ public class UDPReceiver {
                 System.out.println("Pack " + receivedPacks + ": " + message);
                 receivedPacks++;
             }
+        } catch (SocketTimeoutException e){
+            return;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
