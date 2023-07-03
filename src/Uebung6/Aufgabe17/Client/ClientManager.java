fe4c207928bf53681bb3c233963975f0da800ff6
Mathis Steinhage <mathissteinhage@gmx.de>
2023-07-03 16:28:09 +0200

    Aufgabe17: Client

---
+++ src/Uebung6/Aufgabe17/Client/ClientManager.java
@@ -0,0 +1,42 @@
+package Uebung6.Aufgabe17.Client;
+
+import Uebung6.Aufgabe17.Assets.Kreissaver;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientManager extends Thread{
+    private ArrayList<Kreissaver> currentKreise;
+    private BufferedReader reader;
+    private ClientPanel panel;
+    public  ClientManager(ArrayList<Kreissaver> kreise, BufferedReader reader, ClientPanel panel){
+        this.currentKreise = kreise;
+        this.reader = reader;
+        this.panel = panel;
+    }
+
+    @Override
+    public void run(){
+        super.run();
+        while(true){
+            try {
+                if (reader.readLine().equals("New Data")){
+                    while (reader.readLine().equals("Has Next")) {
+                        double kx = Double.parseDouble(reader.readLine());
+                        double ky = Double.parseDouble(reader.readLine());
+                        Color c = new Color(Integer.parseInt(reader.readLine()), Integer.parseInt(reader.readLine()), Integer.parseInt(reader.readLine()));
+                        panel.addKreis(kx,ky,c);
+                    }
+                    panel.repaint();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
--- src/Uebung6/Aufgabe17/Client/ClientPanel.java
+++ src/Uebung6/Aufgabe17/Client/ClientPanel.java
@@ -5,10 +5,12 @@ import Uebung6.Aufgabe17.Assets.Kreissaver;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ClientPanel extends JPanel {
     private ArrayList<Kreissaver> kreise = new ArrayList<>();
-
+    private Lock l = new ReentrantLock();
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -18,7 +20,10 @@ public class ClientPanel extends JPanel {
         }
     }
 
-    public void addKreis(double kx, double ky, Color c) {
+    public synchronized void addKreis(double kx, double ky, Color c) {
+        l.lock();
         kreise.add(new Kreissaver(kx, ky, c));
+        l.unlock();
     }
+    public void deleteKreise(){kreise = new ArrayList<Kreissaver>();}
 }
