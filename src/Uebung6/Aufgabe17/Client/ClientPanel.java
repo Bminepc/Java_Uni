c74e873bdf9d67f4ae354d75442ebb3f11ce1479
Bminepc <benjamin.wen@outlook.com>
2023-07-03 15:06:26 +0200

    Structure

---
+++ src/Uebung6/Aufgabe17/Client/ClientPanel.java
@@ -0,0 +1,24 @@
+package Uebung6.Aufgabe17.Client;
+
+import Uebung6.Aufgabe17.Assets.Kreissaver;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class ClientPanel extends JPanel {
+    private ArrayList<Kreissaver> kreise = new ArrayList<>();
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int i = 0; i < kreise.size(); i++) {
+            g.setColor(kreise.get(i).getColor());
+            g.fillOval((int) kreise.get(i).getX(), (int) kreise.get(i).getY(), 10, 10);
+        }
+    }
+
+    public void addKreis(double kx, double ky, Color c) {
+        kreise.add(new Kreissaver(kx, ky, c));
+    }
+}
--- src/Uebung6/Aufgabe17/Main.java
+++
@@ -1,4 +0,0 @@
-package Uebung6.Aufgabe17;
-
-public class Main {
-}
