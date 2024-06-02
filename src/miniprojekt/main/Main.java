547625d3e9949b424f2086edd05346ae5ac5f3bf
Bminepc <benjamin.wen@outlook.com>
2024-06-02 22:50:14 +0200

    Main für Tests der Liste

--- src/miniprojekt/main/Main.java
+++ src/miniprojekt/main/Main.java
@@ -5,6 +5,17 @@ import miniprojekt.ourList.*;
 public class Main {
 
     public static void main(String[] args) {
+        Listhead<Integer> L = new Listhead<>(1);
+        L.add(2);
+        L.add(3);
+
+        System.out.println(L.getCurrent());
+        L.advance();
+        System.out.println(L.getCurrent());
+        L.advance();
+        System.out.println(L.getCurrent());
+        L.retreat();
+        System.out.println(L.getCurrent());
 
     }
 
