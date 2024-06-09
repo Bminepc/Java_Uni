7ac41a9840c23e10d696898c5ae37c96e159956f
Bminepc <benjamin.wen@outlook.com>
2024-06-09 23:17:40 +0200

    Changed some settings

--- src/miniprojekt/main/Main.java
+++ src/miniprojekt/main/Main.java
@@ -1,22 +1,23 @@
 package miniprojekt.main;
 
-import Uebung4.Aufgabe11.FarbigePixel;
 import miniprojekt.graphics.Spaceframe;
-import miniprojekt.ourList.*;
+import miniprojekt.ourList.Listhead;
 import miniprojekt.spaceobjects.*;
+import miniprojekt.tools.CirclePosition;
+import miniprojekt.tools.Coordinate;
 
 import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Listhead<Spaceobject> spaceobjects = new Listhead<>(new Star("Sonne", 100.0,10.0));
+        Listhead<Spaceobject> spaceobjects = new Listhead<>(new Star("Sonne", 100.0,10.0, 1000));
         spaceobjects.add(new Matterplanet("Erde", spaceobjects.getCurrent(), 20.0,150, Material.STONE, 700000000));
-        spaceobjects.add(new Matterplanet("Erde2", spaceobjects.getCurrent(), 25.0,100, Material.GOLD, 0));
+        spaceobjects.add(new Matterplanet("Erde2", spaceobjects.getCurrent(), 25.0,130, Material.GOLD, 0));
         spaceobjects.add(new Gasplanet("Helo", spaceobjects.getCurrent(), 40, 200, Material.HELIUM));
         spaceobjects.add(new Gasplanet("Helo2", spaceobjects.getCurrent(), 40, 400, Material.HELIUM,0.5));
         try {
-            spaceobjects.add(new Moon("Lunar", spaceobjects.getSpecific(1),25,40,1));
+            spaceobjects.add(new Moon("Lunar", spaceobjects.getSpecific(1),15,40,1));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
