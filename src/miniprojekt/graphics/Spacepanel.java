e941612535d316f65d051cb133655eab28ac1260
Bminepc <benjamin.wen@outlook.com>
2024-06-14 22:13:35 +0200

    Position fixing

--- src/miniprojekt/graphics/Spacepanel.java
+++ src/miniprojekt/graphics/Spacepanel.java
@@ -54,48 +54,48 @@ public class Spacepanel extends JPanel {
 
     public Graphics paintStar(Star s, Graphics g) {
         g.setColor(Color.ORANGE);
-        g.fillOval(s.getPos().getX(), s.getPos().getY(), s.getSize(), s.getSize());
+        g.fillOval(s.getPos().getX() - s.getSize() / 2, s.getPos().getY() - s.getSize() / 2, s.getSize(), s.getSize());
         g.setColor(Color.WHITE);
-        g.drawString(s.getName(), s.getPos().getX(), s.getPos().getY());
+        g.drawString(s.getName(), s.getPos().getX() - s.getSize() / 2, s.getPos().getY() - s.getSize() / 2);
         return g;
     }
 
     public Graphics paintMatterplanet(Matterplanet m, Graphics g) {
         g.setColor(m.getMaterial().getColor());
-        g.fillOval(m.getPos().getX(), m.getPos().getY(), m.getSize(), m.getSize());
+        g.fillOval(m.getPos().getX() - m.getSize() / 2, m.getPos().getY() - m.getSize() / 2, m.getSize(), m.getSize());
         if (m.isInhabitable()) {
             g.setColor(Color.WHITE);
         } else {
             g.setColor(Color.BLACK);
         }
-        g.fillOval(m.getPos().getX() + m.getSize() / 4, m.getPos().getY() + m.getSize() / 4, m.getSize() / 2, m.getSize() / 2);
+        g.fillOval(m.getPos().getX() - m.getSize() / 4, m.getPos().getY() - m.getSize() / 4, m.getSize() / 2, m.getSize() / 2);
         g.setColor(Color.WHITE);
-        g.drawString(m.getName(), m.getPos().getX(), m.getPos().getY());
+        g.drawString(m.getName(), m.getPos().getX() - m.getSize() / 2, m.getPos().getY() - m.getSize() / 2);
         return g;
     }
 
     public Graphics paintGasplanet(Gasplanet p, Graphics g) {
         g.setColor(mapOpacity(p.getMaterial().getColor(), p.getDensity()));
-        g.fillOval(p.getPos().getX(), p.getPos().getY(), p.getSize(), p.getSize());
+        g.fillOval(p.getPos().getX() - p.getSize() / 2, p.getPos().getY() - p.getSize() / 2, p.getSize(), p.getSize());
         g.setColor(Color.WHITE);
-        g.drawString(p.getName(), p.getPos().getX(), p.getPos().getY());
+        g.drawString(p.getName(), p.getPos().getX() - p.getSize() / 2, p.getPos().getY() - p.getSize() / 2);
         return g;
     }
 
     public Graphics paintAsteroid(Asteroid a, Graphics g) {
         g.setColor(Color.MAGENTA);
-        g.fillOval(a.getPos().getX(), a.getPos().getY(), a.getSize(), a.getSize());
+        g.fillOval(a.getPos().getX() - a.getSize() / 2, a.getPos().getY() - a.getSize() / 2, a.getSize(), a.getSize());
         g.drawLine(a.getVector().getStart().getX(), a.getVector().getStart().getY(), a.getVector().getEnd().getX(), a.getVector().getEnd().getY());
         g.setColor(Color.WHITE);
-        g.drawString(a.getName(), a.getPos().getX(), a.getPos().getY());
+        g.drawString(a.getName(), a.getPos().getX() - a.getSize() / 2, a.getPos().getY() - a.getSize() / 2);
         return g;
     }
 
     public Graphics paintMoon(Moon m, Graphics g) {
         g.setColor(mapOpacity(Color.WHITE, m.getReflectiveCoefficient()));
-        g.fillOval(m.getPos().getX(), m.getPos().getY(), m.getSize(), m.getSize());
+        g.fillOval(m.getPos().getX() - m.getSize() / 2, m.getPos().getY() - m.getSize() / 2, m.getSize(), m.getSize());
         g.setColor(Color.WHITE);
-        g.drawString(m.getName(), m.getPos().getX(), m.getPos().getY());
+        g.drawString(m.getName(), m.getPos().getX() - m.getSize() / 2, m.getPos().getY() - m.getSize() / 2);
         return g;
     }
 
