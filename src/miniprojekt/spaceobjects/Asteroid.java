0c569b7eb303c0df114ecb4a8bd3d05770ae2240
Bminepc <benjamin.wen@outlook.com>
2024-06-28 23:13:07 +0200

    Offsetting for diagonals

--- src/miniprojekt/spaceobjects/Asteroid.java
+++ src/miniprojekt/spaceobjects/Asteroid.java
@@ -7,12 +7,15 @@ public class Asteroid extends Spaceobject {
 
     private Vector vector;
     private int direction;
+    private double offsetX, offsetY;
 
     public Asteroid(String name, Vector vector, double size) {
         super(name, null, size);
         this.vector = vector;
         this.position = vector.getStart();
         this.direction = 1;
+        this.offsetX = 0;
+        this.offsetY = 0;
     }
 
     public Vector getVector() {
@@ -26,12 +29,19 @@ public class Asteroid extends Spaceobject {
 
     @Override
     public void advance() {
-        if (position.equals(vector.getEnd())) {
+        if (position.isNear(vector.getEnd())) {
             direction = -1;
-        } else if (position.equals(vector.getStart())) {
+        } else if (position.isNear(vector.getStart())) {
             direction = 1;
         }
-        position = new Coordinate(position.getX() + (int) (vector.getEnd().getX() * 0.01 * direction), position.getY() + (int) (vector.getEnd().getY() * 0.01 * direction));
+        this.offsetX = offsetX + ((vector.getEnd().getX() - vector.getStart().getX()) * 0.01 * direction) - (int) ((vector.getEnd().getX() - vector.getStart().getX()) * 0.01 * direction);
+        this.offsetY = offsetY + ((vector.getEnd().getY() - vector.getStart().getY()) * 0.01 * direction) - (int) ((vector.getEnd().getY() - vector.getStart().getY()) * 0.01 * direction);
+        if (offsetX>1){
+            offsetX = 0;
+        }
+        position = new Coordinate( position.getX() + (int) ((vector.getEnd().getX() - vector.getStart().getX()) * 0.01 * direction) - (offsetX > 1 ? 1 : 0), position.getY() + (int) ((vector.getEnd().getY()-vector.getStart().getY()) * 0.01 * direction) - (offsetY > 1 ? 1 : 0));
+        if (offsetX>1) offsetX = 0;
+        if (offsetY>1) offsetY = 0;
     }
 
     @Override
